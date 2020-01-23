cc804fade5a4accf400f989e09a191fe5b7d6fc1
Benson <jack45j@gmail.com>
2020-01-24 00:55:20 +0800

    Initial Commit

---
+++ StyleGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StyleGuide
+//
+//  Created by Yi-Cheng Lin on 2020/1/24.
+//  Copyright © 2020 Yi-Cheng Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
